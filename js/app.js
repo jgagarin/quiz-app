94ba98e2bd7b82b43bf4ecddb182c455cbb0b781
jgagarin <jon.gagarin@gmail.com>
2015-03-15 14:56:41 -0700

    setup function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    
+  quizSetup();  
 });
 
 var question = new Array();
@@ -12,8 +12,41 @@ function questionSetup(current, answers, correct){
     
 }
 
-question[0]= new questionSetup("Which female artist did The Strokes make a song about modern girls and old fashioned men?",["Kesha","Regina Spektor","Kate Pierson"], 2);
+question[0]= new questionSetup("Which female artist did The Strokes make a song with about modern girls and old fashioned men?",["Kesha","Regina Spektor","Kate Pierson"], 2);
 question[1]= new questionSetup("Which wild popstar did The Strokes collaborate with in 2012?",["Kesha","Rihanna","Santogold"],1);
 question[2]= new questionSetup("Which member's father is the owner of Elite Model Management?",["Julian","Fab","Nick"], 1);
 question[3]= new questionSetup("Which song was removed off the US release of 'This Is It' because of 9/11?",["Barely Legal","One Way Trigger","New York City Cops"],3);
 question[4]= new questionSetup("Which female rock star wrote a love song about Julian?",["Karen O","Courtney Love","Alison Mosshart"], 2);
+
+function quizSetup(){
+    //variables to sort through array
+    var questionCounter = 0;
+    var currentQuestion = question[questionCounter].current;
+    var currentAnswers = question[questionCounter].answers;
+    var currentCorrect = question[questionCounter].correct;
+    
+    //loops through the answers
+    var writeAnswers = function(){ 
+        for( var i = 0; i < currentAnswers.length; i++){
+        var answerIndex = '<li><input type="radio"> '+currentAnswers[i]+'</li>';
+            $('#answers').append(answerIndex).hide().delay(1000).slideDown('slow');    
+        }
+     
+    }
+
+    //writes to the page
+    $('#question').html(currentQuestion).hide().fadeIn('slow');
+    $('#answers').html(writeAnswers);
+  
+    //comparing correct answer
+    
+    
+    function compare(){
+        var a = currentAnswers[currentCorrect];
+        if (blank.value() == currentCorrect){
+            $('.correct').html('Correct!');
+        } else {
+            $('.correct').html(a);
+        }
+    }
+}
